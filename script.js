f18356de677ef6082a96e138f9f9d2098b3e204b
Caina <caina497@gmail.com>
2023-02-18 23:12:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var input = require('fs').readFileSync('./stdin', 'utf8');
+var lines = input.split('\n');
+var columns = input.split(' ');
+
+
+var n = parseInt(lines.shift());
+columns.shift()
+
+
+for (let i = 0; i<n; i++){
+    var f1 = parseInt(lines.shift())
+    var f2 = parseInt(columns.shift())
+
+    /*
+    Resolva o exercício aqui
+    */
+}
